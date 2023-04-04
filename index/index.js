4b3be2390d1b6d3adce1a1f5c5a864298792654a
FacundoMalnero010 <facundomalnero010@gmail.com>
2023-04-04 20:03:47 -0300

    first commit

---
+++ index/index.js
@@ -0,0 +1,12 @@
+const boton = document.getElementById('boton');
+boton.addEventListener('click', function(){
+    setTimeout(redirigirA,500,'../reserva/reserva.php');
+});
+
+function agregarAnimacion(){
+    boton.style.animation = 'wave 0.5s linear';
+}
+
+function redirigirA(pagina){
+    window.location.href = pagina;
+}
