c17c0bef7159c8205da668e39944e6e952aa4e1c
FacundoMalnero <fmalnero@frba.utn.edu.ar>
2023-07-29 23:42:49 -0300

    CSS arreglado

--- index/accesibilidad.js
+++ index/accesibilidad.js
@@ -17,6 +17,11 @@ export function menuSmooth(){
     });
 }
 
+let canScroll  = true;
+const sobreNos = document.getElementById('sobreNosotros');
+const menu     = document.getElementById('menu');
+const cards    = document.querySelectorAll('#sobreNosotros .card');
+
 /**
  * Permite el scrolleo smooth y bloquea momentáneamente el scroll para que la transición se complete
  * 
@@ -24,49 +29,50 @@ export function menuSmooth(){
  */
 
 export function scrollPausadoSmooth(event){
-  let canScroll = true;
   // previene el desplazamiento predeterminado del mouse
   event.preventDefault();
   // si no se puede hacer scroll, salimos de la función
-  if (!canScroll) return;
-  // obtiene la dirección del desplazamiento del mouse
-  const direction = event.deltaY > 0 ? 1 : -1;
-  // establece un tiempo de espera antes de poder hacer scroll de nuevo3
-  canScroll = false;
-  setTimeout(() => (canScroll = true), 1000);
-  // obtiene la siguiente sección
-  const currentSection = event.target.closest(".seccion");
-  const nextSection = currentSection.nextElementSibling;
-  // si hay una siguiente sección y la dirección es hacia abajo, desplázate a ella con un efecto suave
-  if (nextSection && direction === 1) {
-    nextSection.scrollIntoView({behavior: "smooth", duration: 1000});
-  }
-  // si hay una sección anterior y la dirección es hacia arriba, desplázate a ella con un efecto suave
-  if (currentSection.previousElementSibling && direction === -1) {
-    currentSection.previousElementSibling.scrollIntoView({behavior: "smooth",duration: 1000});
+  if (canScroll){
+    // obtiene la dirección del desplazamiento del mouse
+    const direction = event.deltaY > 0 ? 1 : -1;
+    // establece un tiempo de espera antes de poder hacer scroll de nuevo3
+    canScroll = false;
+    setTimeout(() => scrolleoEntreSecciones(sobreNos,menu,cards), 250);
+    setTimeout(() => (canScroll = true), 1000);
+    // obtiene la siguiente sección
+    const currentSection = event.target.closest(".seccion");
+    // si hay una siguiente sección y la dirección es hacia abajo, desplázate a ella con un efecto suave
+    if (currentSection.nextElementSibling && direction === 1) {
+      const nextSection = currentSection.nextElementSibling;
+      nextSection.scrollIntoView({behavior: "smooth", duration: 1000});
+    }
+    // si hay una sección anterior y la dirección es hacia arriba, desplázate a ella con un efecto suave
+    if (currentSection.previousElementSibling && direction === -1) {
+      currentSection.previousElementSibling.scrollIntoView({behavior: "smooth",duration: 1000});
+    }
   }
 }
 
+
+let animacionCards = false, animacionMenu = false;
+
 /**
- * Hace que el scrolleo pase de una sección a otra
+ * Maneja las animaciones de los elementos de las secciones
+ * 
+ * @param {HTMLElement}   sobreNos
+ * @param {HTMLElement}   menu
+ * @param {HTMLElement[]} cards
  */
 
-export function scrolleoEntreSecciones(){
-    let animacionCards = false, animacionMenu = false;
-    const sobreNos = document.getElementById('sobreNosotros');
-    const menu     = document.getElementById('menuImage');
-    const cards    = document.querySelectorAll('#sobreNosotros .card');
-
-    // verifico la posicion actual de la pantalla
-    const position = window.scrollY;
-
-    // si me encuentro en la seccion 2, le agrego animación
-    if(position <= sobreNos.offsetTop-1 && !animacionCards){
+function scrolleoEntreSecciones(sobreNos,menu,cards){
+    // Verifico la sección para manejar la animación a agregar
+    if(position < sobreNos.offsetTop && !animacionCards){
         cards.forEach(card => {card.classList.add('deslizar')});
         animacionCards = true;
     }
-    if(position >= sobreNos.offsetTop+1 && !animacionMenu){
-        menu.classList.add('agrandar');
+    if(position < menu.offsetTop && position > sobreNos.offsetTop && !animacionMenu){
+        const menuImage = document.getElementById('menuImage');
+        menuImage.classList.add('agrandar');
         animacionMenu = true;
     }
 }
